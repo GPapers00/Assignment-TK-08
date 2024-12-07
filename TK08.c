52342dab703bf958ce027c66bd6428e6032148ad
GPapers00 <liandryo05@gmail.com>
2024-12-07 19:31:57 +0700

    Delete TK08.c

--- TK08.c
+++
@@ -1,9 +0,0 @@
-float numAdder(float num1, float num2){
-    num1 + num2;
-    return num1 = num1 + num2;
-}
-
-float numMultiplier(float num1, float num2){
-    num1 * num2;
-    return num1 = num1 * num2;
-}
