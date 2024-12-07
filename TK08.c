5e2a0ed070a8024412e60f85bf7c5ded4ce82a5f
Koecheng <koechengez3rd@gmail.com>
2024-12-07 19:41:49 +0700

    Finale.c

--- TK08.c
+++ TK08.c
@@ -1,7 +1,52 @@
+#include <stdio.h>
+
 float numAdder(float num1, float num2){
     return num1 = num1 + num2;
 }
 
 float numMultiplier(float num1, float num2){
     return num1 = num1 * num2;
+}
+
+float numSubstractor(float num1, float num2) {
+    return num1 = num1 - num2;
+}
+
+float numDivider(float num1, float num2) {
+    return num1 = num1/num2;
+}
+
+int main() {
+    int option = 1;
+    float num1, num2;
+    while(option > 0) {
+        printf("Welcome to the Calculator of mankind.\n");
+        printf("Options :\n1. Addition\n2. Substraction\n3. Multiplication\n4. Division\n");
+        printf("Enter 0 to end the program.\n");
+        scanf("%d", &option);
+        printf("Enter the 1st Number : ");
+        scanf("%f", &num1);
+        printf("\nEnter the 2nd Number : ");
+        scanf("%f", &num2);
+
+
+        switch(option) {
+            case '0':
+            return 0;
+            
+            case '1':
+            numAdder(num1, num2);
+
+            case '2':
+            numSubstractor(num1, num2);
+
+            case '3':
+            numMultiplier(num1, num2);
+
+            case '4':
+            numDivider(num1, num2);
+        }
+
+        printf("Result : %.1f", num1);
+    }
 }
